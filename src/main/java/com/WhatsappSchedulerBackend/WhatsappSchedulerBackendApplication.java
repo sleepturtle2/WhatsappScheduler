b9b4e0a1a4f750d88fefb77200ca66c34c8fb230
Sayantan Mukherjee <sayantan.mukh98@gmail.com>
2022-05-26 02:01:59 +0530

    commit

---
+++ src/main/java/com/WhatsappSchedulerBackend/WhatsappSchedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.WhatsappSchedulerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappSchedulerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappSchedulerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WhatsappSchedulerBackend/WhatsappSchedulerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WhatsappSchedulerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappSchedulerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
