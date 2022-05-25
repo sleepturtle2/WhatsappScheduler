19f36db68cfb53b1f5b8242999abce8ff39a73b9
Sayantan Mukherjee <sayantan.mukh98@gmail.com>
2022-05-26 03:06:16 +0530

    added entity class;

---
+++ src/main/java/com/WhatsappSchedulerBackend/model/Message.java
@@ -0,0 +1,32 @@
+package com.WhatsappSchedulerBackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="Message")
+public class Message {
+    @Id
+    @Column(name = "message_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+    private String messageBody;
+    private LocalDateTime scheduledDateTime;
+    private Long receiverPhoneNumber;
+    private Long clientId;
+    private LocalDateTime creationDateTime;
+    private Integer messageStatus;
+    private LocalDateTime submittedDateTime;
+    private String gupshupMessageId;
+
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+}
